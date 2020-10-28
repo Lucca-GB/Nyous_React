546a672ede000e073ea4cb14c9deb8565c29b8ee
Your Name <you@example.com>
2020-10-27 21:09:33 -0300

    Paginas login, cadastrar e home finalizadas e rota difinida

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Home from './pages/home';
+import Login from './pages/login';
+import Cadastrar from './pages/cadastrar';
+import NaoEncontrada from './pages/naoencontrado';
+
+
+import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/login' component={Login} />
+      <Route path='/cadastrar' component={Cadastrar} />
+      <Route component={NaoEncontrada} />
+    </Switch>
+  </Router>
+)
+
+
+ReactDOM.render(
+  routing,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Menu from '../../components/menu';
+import Rodape from '../../components/rodape';
+import { Container, Form, Button } from 'react-bootstrap';
+import logo from '../../assets/img/sun.png';
+import './index.css';
+
+const Login = () => {
+    return (
+        <div>
+            <Menu />
+                <Container className='form-height'>
+                    <Form className='form-signin'>
+                        <div className='text-center'>
+                            <img src={logo} alt="NYOUS" style={{ width : '64px'}}/>
+                        </div>
+                        <br/>
+                        <small>Informe os dados abaixo</small>
+                        <hr/>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type='email' placeholder='Informe o seu email' required></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type='password' placeholder='Senha' required></Form.Control>
+                        </Form.Group>
+                        <Button>
+                            Enviar
+                        </Button>
+                        <br/><br/>
+                        <a href="/cadastrar" style={{ marginTop : '30px'}}>Nao tenho conta</a>
+                        
+                    </Form>    
+                </Container>
+            <Rodape />
+        </div>
+    )
+}
+
+export default Login;
