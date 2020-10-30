5e76780a212f6c9de896f58c67abd61a03143d9c
Your Name <you@example.com>
2020-10-30 00:29:51 -0300

    crud e paginas de admin finalizadas. criacao de rotas

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import jwt_decode from 'jwt-decode';
+import { useHistory } from 'react-router-dom';
 
 const Menu = () => {
+
+    const history = useHistory();
+
+    const sair = (event) => {
+        event.preventDefault();
+
+        localStorage.removeItem('token-nyous-tarde');
+
+        history.push('/')
+    }
+
+    const renderMenu = () => {
+        const token = localStorage.getItem('token-nyous-tarde');
+
+        if (token === null) {
+            return (
+                <Nav>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/cadastrar">Cadastrar</Nav.Link>
+                </Nav>
+            );
+        } else if (jwt_decode(token).role === 'Admin') {
+            return (
+                <Nav>
+                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link href="/eventos">Eventos</Nav.Link>
+                    <Nav.Link href="/categorias">Categorias</Nav.Link>
+                    <Nav.Link href="/sair">Sair</Nav.Link>
+                    <NavDropdown title={jwt_decode(token).family_name} id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/perfil">Perfil</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item onClick={event => sair(event)}>Sair</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            )
+        } else {
+            return (
+                <Nav>
+                    <Nav.Link href="/eventos">Eventos</Nav.Link>
+                    <NavDropdown title={jwt_decode(token).family_name} id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/perfil">Perfil</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item onClick={event => sair(event)}>Sair</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            )
+        }
+    }
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Brand href="/">Nyous</Navbar.Brand>
@@ -10,10 +61,8 @@ const Menu = () => {
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                 </Nav>
-                <Nav>
-                    <Nav.Link href= "/login">Login</Nav.Link>
-                    <Nav.Link href= "/cadastrar">Cadastrar</Nav.Link>
-                </Nav>
+
+                {renderMenu()}
             </Navbar.Collapse>
         </Navbar>
     )
--- src/pages/admin/crudcategorias/index.jsx
+++ src/pages/admin/crudcategorias/index.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, {useEffect}from 'react';
 import Menu from '../../../components/menu';
 import Rodape from '../../../components/rodape';
 
-const DashBoard = () => {
-    
+
+const CrudCategorias = () => {
+
+
     return (
         <div>
             <Menu/>
-            <h1>Eventos</h1>
+            <h1>CrudCategorias</h1>
             <Rodape/>
         </div>
     )
 }
 
-export default DashBoard;
+export default CrudCategorias;
